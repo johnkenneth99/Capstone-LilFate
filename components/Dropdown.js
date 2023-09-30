887d7d76cc982e9ab1909b8a0e79c879ed202b1c
Kenneth Sinay <jksinay@gmail.com>
2023-09-30 15:06:26 +0800

    refs #master
    Updated DropDown style
    Used useId hook for DropDown component

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -1,23 +1,27 @@
 import classNames from "classnames";
 import { useField } from "formik";
-import React, { Fragment } from "react";
+import { Fragment, useId } from "react";
 import { ErrorMessage } from "formik";
 
 export default function Dropdown({ name = null, label = null, options = [], disabled = false, ...props }) {
+  const id = useId();
   const [field, { error, touched }] = useField(name);
 
   const labelClass = classNames("top-6", "left-2 ", "relative", "text-sm", "font-medium", { "text-red-500": !!error && touched });
 
   return (
     <div className="flex flex-col w-full focus-within:text-blue-400 mb-0">
-      <label className={labelClass}>{label}</label>
+      <label className={labelClass} for={id}>
+        {label}
+      </label>
       <select
         className={classNames(
-          "pt-5 pb-2 px-2 outline-0 border rounded focus:rounded-none focus:border-0 focus:border-blue-400 focus:border-b-[1px]",
-          { "text-slate-500": disabled },
-          { "text-black": !disabled },
+          "pt-5 pb-2 px-2 outline-0 border rounded text-black focus:rounded-none focus:border-0 focus:border-blue-400 focus:border-b-[1px]",
+          { "opacity-100": disabled },
+          { "bg-gray-100": disabled },
           { "border-red-500": !!error && touched }
         )}
+        id={id}
         disabled={disabled}
         {...props}
         {...field}
