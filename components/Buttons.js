214016eff740fe0c87c954cf48134f0a21184e2b
Kenneth Sinay <jksinay@gmail.com>
2023-10-15 15:42:14 +0800

    refs #master
    Added disabled style for PrimaryButton component

--- components/Buttons.js
+++ components/Buttons.js
@@ -1,6 +1,15 @@
-export function PrimaryButton({ label = "", icon, ...props }) {
+import classNames from "classnames";
+
+export function PrimaryButton({ width = null, height = null, label = "", icon, disabled = false, ...props }) {
+  const buttonClass = classNames(
+    "flex justify-center items-center rounded font-medium p-3 gap-1",
+    { "text-white bg-slate-600 hover:bg-slate-400": !disabled },
+    { "text-slate-500 bg-slate-400": !!disabled },
+    { [width]: !!width },
+    { [height]: !!height }
+  );
   return (
-    <button className="flex justify-center items-center bg-slate-600 rounded text-white font-medium p-3 gap-1 hover:bg-slate-400" {...props}>
+    <button className={buttonClass} disabled={disabled} {...props}>
       {icon}
       {label}
     </button>
